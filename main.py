0c7b771f8f21800cc1a37071370a32d4c5accd36
Hsin-Wei Wang <wanghsinwei@msn.com>
2019-07-16 05:42:54 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,101 @@
+import argparse
+import os
+from keras.applications.densenet import DenseNet201, preprocess_input as preprocess_input_densenet
+from keras.applications.xception  import Xception, preprocess_input as preprocess_input_xception
+from keras.applications.nasnet import NASNetLarge, preprocess_input as preprocess_input_nasnet
+from keras import backend as K
+from keras.utils import np_utils
+from data import load_isic_data, train_validation_split, compute_class_weight_dict
+from vanilla_classifier import VanillaClassifier
+from transfer_learn_classifier import TransferLearnClassifier
+from metrics import balanced_accuracy
+from base_model_param import BaseModelParam
+
+def main():
+    parser = argparse.ArgumentParser(description='ISIC-2019 Skin Lesion Classifiers')
+    parser.add_argument('data', metavar='DIR', help='path to data foler')
+    parser.add_argument('--batchsize', type=int, help='Batch size (default: %(default)s)', default=40)
+    parser.add_argument('--epoch', type=int, help='Number of epochs', required=True)
+    model_group = parser.add_mutually_exclusive_group(required=True)
+    model_group.add_argument('--vanilla', dest='vanilla', action='store_true', help='Vanilla CNN')
+    model_group.add_argument('--transfer', dest='transfer_learning', action='store_true', help='Transfer Learning')
+    args = parser.parse_args()
+    # print(args)
+    # return
+
+    data_folder = args.data
+    batch_size = args.batchsize
+    epoch_num = args.epoch
+
+    derm_image_folder = os.path.join(data_folder, 'ISIC_2019_Training_Input')
+    ground_truth_file = os.path.join(data_folder, 'ISIC_2019_Training_GroundTruth.csv')
+    df_ground_truth, category_names = load_isic_data(derm_image_folder, ground_truth_file)
+    df_train, df_val = train_validation_split(df_ground_truth)
+    class_weight_dict = compute_class_weight_dict(df_train)
+
+    if args.vanilla:
+        train_vanilla(df_train, df_val, len(category_names), class_weight_dict, batch_size, epoch_num)
+    else:
+        train_transfer_learning(df_train, df_val, len(category_names), class_weight_dict, batch_size, epoch_num)
+
+def train_vanilla(df_train, df_val, known_category_num, class_weight_dict, batch_size, epoch_num):
+    input_size = (224, 224)
+    rescale=1./255
+    workers = os.cpu_count()
+
+    classifier = VanillaClassifier(
+        input_size=input_size,
+        image_data_format=K.image_data_format(),
+        num_classes=known_category_num,
+        batch_size=batch_size,
+        rescale=rescale,
+        metrics=[balanced_accuracy, 'accuracy'],
+        image_paths_train=df_train['path'].tolist(),
+        categories_train=np_utils.to_categorical(df_train['category'], num_classes=known_category_num),
+        image_paths_val=df_val['path'].tolist(),
+        categories_val=np_utils.to_categorical(df_val['category'], num_classes=known_category_num)
+    )
+    classifier.model.summary()
+    classifier.train(epoch_num=epoch_num, class_weight=class_weight_dict, workers=workers)
+
+
+def train_transfer_learning(df_train, df_val, known_category_num, class_weight_dict, batch_size, epoch_num):
+    workers = os.cpu_count()
+
+    base_model_params = [
+        BaseModelParam(module_name='keras.applications.densenet',
+                    class_name='DenseNet201',
+                    input_size=(224, 224),
+                    layers_trainable=False,
+                    preprocessing_func=preprocess_input_densenet),
+        BaseModelParam(module_name='keras.applications.xception',
+                    class_name='Xception',
+                    input_size=(299, 299),
+                    layers_trainable=False,
+                    preprocessing_func=preprocess_input_xception),
+        BaseModelParam(module_name='keras.applications.nasnet',
+                    class_name='NASNetLarge',
+                    input_size=(331, 331),
+                    layers_trainable=False,
+                    preprocessing_func=preprocess_input_nasnet)
+    ]
+
+    for model_param in base_model_params:
+        classifier = TransferLearnClassifier(
+            base_model_param=model_param,
+            fc_layers=[1024],
+            num_classes=known_category_num,
+            dropout=0.3,
+            batch_size=batch_size,
+            image_data_format=K.image_data_format(),
+            metrics=[balanced_accuracy, 'accuracy'],
+            image_paths_train=df_train['path'].tolist(),
+            categories_train=np_utils.to_categorical(df_train['category'], num_classes=known_category_num),
+            image_paths_val=df_val['path'].tolist(),
+            categories_val=np_utils.to_categorical(df_val['category'], num_classes=known_category_num)
+        )
+        # classifier.model.summary()
+        classifier.train(epoch_num=epoch_num, class_weight=class_weight_dict, workers=workers)
+
+if __name__ == '__main__':
+    main()
