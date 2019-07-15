b0ef197ab4d2f52a59c7b8f22557586def4bfc6a
Hsin-Wei Wang <wanghsinwei@msn.com>
2019-07-15 22:48:19 +0800

    Change the way of defining typing.NamedTuple to support Python 3.5

--- base_model_param.py
+++ base_model_param.py
@@ -1,9 +1,10 @@
 from typing import NamedTuple
 from types import FunctionType
 
-class BaseModelParam(NamedTuple):
-    module_name: str
-    class_name: str
-    input_size: tuple
-    layers_trainable: bool
-    preprocessing_func: FunctionType
+BaseModelParam = NamedTuple('BaseModelParam', [
+    ('module_name', str),
+    ('class_name', str),
+    ('input_size', tuple),
+    ('layers_trainable', bool),
+    ('preprocessing_func', FunctionType)
+])
