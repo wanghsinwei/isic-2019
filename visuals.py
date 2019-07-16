cf93a26887bd0824153b2db8fd1cc0f066f722c4
Hsin-Wei Wang <wanghsinwei@msn.com>
2019-07-16 20:30:18 +0800

    Add bar charts for train_validation_split and class weights.

--- visuals.py
+++ visuals.py
@@ -17,3 +17,16 @@ def plot_complexity_graph(csv_file, figsize=(20, 5)):
     ax2.plot(df['val_balanced_accuracy'], label='Validation Accuracy')
     ax2.set(xlabel='epoch', ylabel='Balanced Accuracy')
     ax2.legend()
+
+def autolabel(ax, rects):
+    """
+    Attach a text label above each bar in *rects*, displaying its height.
+    Ref: https://matplotlib.org/3.1.1/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
