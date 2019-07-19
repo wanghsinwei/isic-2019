a39db19da46d7cb09e271abc3e63e0c9fbc0e601
Hsin-Wei Wang <wanghsinwei@msn.com>
2019-07-19 23:53:38 +0800

    Change the Image Augmentation Pipeline for Training Set.

--- lesion_classifier.py
+++ lesion_classifier.py
@@ -41,14 +41,28 @@ class LesionClassifier():
     @staticmethod
     def create_aug_pipeline_train(input_size):
         """Image Augmentation Pipeline for Training Set."""
+        # p_train = Pipeline()
+        # # Resize the image to 1.25 times of the desired input size of the model
+        # resize_target_size = tuple(math.ceil(1.25*x) for x in input_size)
+        # p_train.resize(probability=1, width=resize_target_size[0], height=resize_target_size[1])
+        # # Random crop
+        # p_train.add_operation(CropPercentageRange(probability=1, min_percentage_area=0.8, max_percentage_area=1, centre=False))
+        # # Resize the image to the desired input size of the model
+        # p_train.resize(probability=1, width=input_size[0], height=input_size[1])
+        # # Rotate the image by either 90, 180, or 270 degrees randomly
+        # p_train.rotate_random_90(probability=0.5)
+        # # Flip the image along its vertical axis
+        # p_train.flip_top_bottom(probability=0.5)
+        # # Flip the image along its horizontal axis
+        # p_train.flip_left_right(probability=0.5)
+        # # Random change brightness of the image
+        # p_train.random_brightness(probability=0.5, min_factor=0.9, max_factor=1.1)
+        # # Random change saturation of the image
+        # p_train.random_color(probability=0.5, min_factor=0.9, max_factor=1.1)
+
         p_train = Pipeline()
-        # Resize the image to 1.25 times of the desired input size of the model
-        resize_target_size = tuple(math.ceil(1.25*x) for x in input_size)
-        p_train.resize(probability=1, width=resize_target_size[0], height=resize_target_size[1])
         # Random crop
         p_train.add_operation(CropPercentageRange(probability=1, min_percentage_area=0.8, max_percentage_area=1, centre=False))
-        # Resize the image to the desired input size of the model
-        p_train.resize(probability=1, width=input_size[0], height=input_size[1])
         # Rotate the image by either 90, 180, or 270 degrees randomly
         p_train.rotate_random_90(probability=0.5)
         # Flip the image along its vertical axis
@@ -59,6 +73,9 @@ class LesionClassifier():
         p_train.random_brightness(probability=0.5, min_factor=0.9, max_factor=1.1)
         # Random change saturation of the image
         p_train.random_color(probability=0.5, min_factor=0.9, max_factor=1.1)
+        # Resize the image to the desired input size of the model
+        p_train.resize(probability=1, width=input_size[0], height=input_size[1])
+
         return p_train
 
     @staticmethod
